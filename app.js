dab43dd57687ade98b2a9d94ee587ef95c399e72
@Ash9311 <ashutoshmulki7@gmail.com>
2022-03-19 17:12:06 +0530

    DB and login setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const { use } = require("express/lib/router");
 
 const app = express();
 
@@ -11,6 +12,15 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User",userSchema);
 
 //TODO
 
@@ -24,6 +34,23 @@ app.get("/login",function(req,res){
 
 app.get("/register",function(req,res){
     res.render("register");
+})
+
+app.post("/register",function(req,res){
+    const newUser = new User({
+        email:req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save(function(err){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render("secrets");
+        }
+    })
+
 })
 
 
