4601efa7cdbbf2cd8e591ca577d87405dcfad286
@Ash9311 <ashutoshmulki7@gmail.com>
2022-04-07 23:44:29 +0530

    google login

--- app.js
+++ app.js
@@ -38,7 +38,8 @@ mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -48,8 +49,15 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser(function (id, done) {
+    User.findById (id, function(err, user) {
+      done(err, user);
+    });
+  });
 
 passport.use(new GoogleStrategy({
     clientID:     process.env.CLIENT_ID,
@@ -60,7 +68,7 @@ passport.use(new GoogleStrategy({
   },
   function(request, accessToken, refreshToken, profile, done) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return cb(err, user);
+      return  done(err, user);
     });
   }
 ));
@@ -71,9 +79,9 @@ app.get("/",function(req,res){
     res.render("home");
 })
 
-app.get("/auth/google",function(req,res){
-    passport.authenticate("google", {scope: ["profile"]});
-})
+app.get("/auth/google", passport.authenticate('google', { 
+    scope: ['profile']
+}));
 
 app.get('/auth/google/secrets',
   passport.authenticate('google', { failureRedirect: '/login' }),
