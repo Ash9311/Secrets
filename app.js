4fd9e493efb55aca39bc3b944b204ea5c28e95e6
@Ash9311 <ashutoshmulki7@gmail.com>
2022-04-09 13:57:14 +0530

    finishing up

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -66,6 +67,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
     passReqToCallback   : true
   },
+  
   function(request, accessToken, refreshToken, profile, done) {
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return  done(err, user);
@@ -99,12 +101,41 @@ app.get("/register",function(req,res){
 })
 
 app.get("/secrets",function(req,res){
+    User.find({"secret": {$ne: null}},function(err,foundUsers){
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUsers){
+                res.render("secrets",{usersWithSecrets: foundUsers});
+            }
+        }
+    })
+});
+
+app.get("/submit",function(req,res){
     if(req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     }
     else{
-        res.render("/login");
+        res.redirect("/login");
     }
+})
+
+app.post("/submit",function(req,res){
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id,function(err,foundUser){
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
 })
 
 app.get("/logout",function(req,res){
